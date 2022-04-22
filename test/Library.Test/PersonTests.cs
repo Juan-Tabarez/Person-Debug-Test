20ec7093cd796cfe9fee994109d44e5b84e37f4f
Juan-Tabarez <jtabarez6@gmail.com>
2022-04-22 17:21:59 -0300

    version1

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -12,10 +12,53 @@ namespace Tests
             // Insertá tu código de inicialización aquí
         }
 
+        //Test de nombres validos
+
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
-        {
-            // Insertá tu código  de pruebaaquí
+        public void validNameTest1() 
+        {   
+            Person person = new Person("Juan","51369506");
+            string expected = "Juan";
+            string actual = person.Name;
+            Assert.AreEqual(expected,actual);
         }
+        
+        [Test]
+        public void validNameTest2() 
+        {   
+            Person person = new Person("Juan Román Riquelme","51369506");
+            string expected = "Juan Román Riquelme";
+            string actual = person.Name;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void validNameTest3() 
+        {   
+            Person person = new Person("a","51369506");
+            string expected = "a";
+            string actual = person.Name;
+            Assert.AreEqual(expected,actual);
+        }
+
+        //Test de nombres no validos
+
+        [Test]
+        public void noValidNameTest1() 
+        {   
+            Person person = new Person("","51369506");
+            string expected = null;
+            string actual = person.Name;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void noValidNameTest2() 
+        {   
+            Person person = new Person(null,"51369506");
+            string expected = null;
+            string actual = person.Name;
+            Assert.AreEqual(expected,actual);
+        }     
     }
 }
