f0c3770a7b6940a12f73a2b373c4ba2e6c413b49
Juan-Tabarez <jtabarez6@gmail.com>
2022-04-22 20:08:40 -0300

    version2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,7 @@ namespace UnitTestAndDebug
     {
         static void Main(string[] args)
         {
-            Person john = new Person("John Doe", "51369506");
+            Person john = new Person("John Doe", "5.136.950-6");
             Person jane = new Person("Jane Doe", "8.765.432-1");
             john.IntroduceYourself();
             jane.IntroduceYourself();
--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -12,7 +12,7 @@ namespace Tests
             // Insertá tu código de inicialización aquí
         }
 
-        //Test de nombres validos
+        //Tests de nombres validos
 
         [Test]
         public void validNameTest1() 
@@ -41,7 +41,7 @@ namespace Tests
             Assert.AreEqual(expected,actual);
         }
 
-        //Test de nombres no validos
+        //Tests de nombres no validos
 
         [Test]
         public void noValidNameTest1() 
@@ -59,6 +59,108 @@ namespace Tests
             string expected = null;
             string actual = person.Name;
             Assert.AreEqual(expected,actual);
-        }     
+        }  
+
+        //Tests de cedulas validas
+
+        [Test]
+        public void validIdTest1()
+        {
+            Person person = new Person("Juan", "51369506");
+            string expected = "51369506";
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        } 
+
+        [Test]
+        public void validIdTest2()
+        {
+            Person person = new Person("Juan", "5.136.950-6");
+            string expected = "5.136.950-6";
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void validIdTest3()
+        {
+            Person person = new Person("Juan", "5.136.950.6");
+            string expected = "5.136.950.6";
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void validIdTest4()
+        {
+            Person person = new Person("Juan", "5-136-950-6");
+            string expected = "5-136-950-6";
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        } 
+
+        //Tests de cedulas no validas
+
+        [Test]
+        public void noValidIdTest1()
+        {
+            Person person = new Person("Juan", "8.765.432-1");
+            string expected = null;
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void noValidIdTest2()
+        {
+            Person person = new Person("Juan", "8.765.432");
+            string expected = null;
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void noValidIdTest3()
+        {
+            Person person = new Person("Juan", "876543210");
+            string expected = null;
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void noValidIdTest4()
+        {
+            Person person = new Person("Juan", "5");
+            string expected = null;
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void noValidIdTest5()
+        {
+            Person person = new Person("Juan", "");
+            string expected = null;
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void noValidIdTest6()
+        {
+            Person person = new Person("Juan", null);
+            string expected = null;
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
+
+        public void noValidIdTest7()
+        {
+            Person person = new Person("Juan", "ghcvzsgufds");
+            string expected = null;
+            string actual = person.ID;
+            Assert.AreEqual(expected,actual);
+        }
     }
 }
